b6c313b3c776c7dc54f8b1ff42123c2820b31084
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-01-19 17:08:16 -0800

    connecting sqlite database with the server

--- Guatemala/app.js
+++ Guatemala/app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 // SQLite database connection
 const db = new sqlite3.Database('Population.db', (err) => {
@@ -16,36 +17,14 @@ const db = new sqlite3.Database('Population.db', (err) => {
   }
 });
 
-// Define your table schema and create it if not exists
-db.run(`
-  CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
-  )
-`);
-
-// Express route to insert user into the database
-app.post('/api/users', (req, res) => {
-  const { name } = req.body;
-
-  db.run('INSERT INTO users (name) VALUES (?)', [name], function (err) {
+// Express route to fetch population data from the database
+app.get('/api/populationData', (req, res) => {
+  db.all('SELECT * FROM total_population', (err, rows) => {
     if (err) {
-      console.error('Error inserting user into the database:', err.message);
+      console.error('Error fetching population data:', err.message);
       res.status(500).json({ error: 'Internal Server Error' });
     } else {
-      res.json({ message: 'User inserted successfully' });
-    }
-  });
-});
-
-// Express route to fetch all users from the database
-app.get('/api/users', (req, res) => {
-  db.all('SELECT * FROM users', (err, rows) => {
-    if (err) {
-      console.error('Error fetching users from the database:', err.message);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json({ users: rows });
+      res.json({ populationData: rows });
     }
   });
 });
