8f0ff5031eb07a98449ca57e3e932c457555cb53
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-06 20:56:25 -0800

    creating sum.js file

---
+++ Daily_practices/sum.js
@@ -0,0 +1,5 @@
+// a code that sums two numbers you choose in the terminal, the numbers are passed as arguments and the result is printed in the terminal
+// Example: node sum.js 1 2
+// Output: 3
+const args = process.argv;
+console.log(args);
