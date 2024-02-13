d205d464da0e6363e5a27206f34ef9e8894d54cb
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-12 18:19:57 -0800

    adding the logical one

---
+++ Daily_practices/conditional_if.js
@@ -0,0 +1,10 @@
+// Create a function using the if conditional statement to evaluate 2 options and print the result in the terminal within another if function
+const MoreIcrecream = true;
+const MorePoutine = true;
+
+if (MoreIcrecream) {
+  console.log("I will stick with more icecream");
+} 
+else if (MorePoutine) {
+  console.log("I will stick with more poutine");
+}
