29c399d7afbc1acf44db2cab8243ed15d5d163d0
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-12 17:12:30 -0800

    adding indented_code

---
+++ Daily_practices/function_indented_code.js
@@ -0,0 +1,6 @@
+//create a function with indented code using 2 spaces
+function example() {
+//inside of the function is where we put the indented code
+  console.log("This is the indented code")
+}
+example();
