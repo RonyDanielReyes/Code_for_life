007ef7b66294e8f0e9b3c0f3f2aba2741c09c6ec
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-14 00:30:24 -0800

    editing some files

--- Daily_practices/3. conditional_if.js
+++ Daily_practices/3. conditional_if.js
@@ -1,4 +1,9 @@
 // Create a function using the if conditional statement to evaluate 2 options and print the result in the terminal within another if function
+// if MoreIcrecream is true, it will print "I will stick with more icecream"
+// if MorePoutine is true, it will print "I will stick with more poutine"
+// if both are false, it will print "I will stick with more salad"
+
+
 const MoreIcrecream = false;
 const MorePoutine = true;
 
--- Daily_practices/4. conditional_2.js
+++ Daily_practices/4. conditional_2.js
@@ -1,5 +1,7 @@
 // create a program that put cold and raining as a condition to go outside
 // if raining is true, it will print "Don't forget your umbrella!"
+// if cold is true, it will print "Make sure you pick out a scarf!"
+// if both are false, it will print "Now you're ready to go outside!"
 
 const raining = true;
 const cold = false;
