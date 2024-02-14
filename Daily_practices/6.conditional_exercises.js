b2c738e1bb4d2fa8907ce1c61ebca9e2f69a72ee
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-14 01:01:38 -0800

    exercise 1

---
+++ Daily_practices/6.conditional_exercises.js
@@ -0,0 +1,11 @@
+// write a statement that checks if a number is positive, if it is print a positive number to the console
+
+let number = 1;
+
+if (number > 0) {
+  console.log("This is a positive number");
+} else {
+  console.log("This is a negative number");
+}
+
+//
