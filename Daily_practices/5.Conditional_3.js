6b11b6739535f48d7ecdda25bf3ffbb3816332b8
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-14 00:53:57 -0800

    adding exeercise number 5

--- Daily_practices/5.Conditional_3.js
+++ Daily_practices/5.Conditional_3.js
@@ -1,7 +1,14 @@
-const cold = false;
+// create a program where a variable is created called temperature and is set to 12.
+// if the temperature is less than 0, it will print "Make sure you pick out a scarf!"
+// if the temperature is greater than 15, it will print "Don't forget your sunglasses!"
+// if the temperature is less than 15, it will print "Now you're ready to go outside!"
 
-if (cold) {
+const temperature = -16;
+
+if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
+} else if (temperature > 15) {
+  console.log("Don't forget your sunglasses!");
 } else {
-  console.log("Short sleeves are fine.");
+  console.log("Now you're ready to go outside!");
 }
