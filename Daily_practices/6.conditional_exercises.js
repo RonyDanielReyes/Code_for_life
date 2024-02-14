22e3194d94658e2744bf5a17f9b945504e07fb16
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-14 01:07:18 -0800

    adding last challengue

--- Daily_practices/6.conditional_exercises.js
+++ Daily_practices/6.conditional_exercises.js
@@ -8,4 +8,19 @@ if (number > 0) {
   console.log("This is a negative number");
 }
 
-//
+// write a statement that checks if a person is old enough to vote, if their age is 18 or above, print "You are old enough to vote"
+// if they are 16 and 17 print "You are almost old enough to vote"
+// if they are 14 and 15 print "You are 4-5 years close to vote"
+// if they are 13 or below print "You are not old enough to vote"
+
+let age = 19;
+
+if (age >= 18) {
+  console.log("You are old enough to vote");
+} else if (age >= 16 && age <= 17) {
+  console.log("You are almost old enough to vote");
+} else if (age >= 14 && age <= 15) {
+  console.log("You are 4-5 years close to vote");
+} else (age <= 13); {
+  console.log("You are not old enough to vote");
+}
