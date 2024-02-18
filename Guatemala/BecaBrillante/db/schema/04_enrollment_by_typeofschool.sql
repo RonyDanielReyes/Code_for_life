d4797fac8f71c1489b6b7492b46b10ef6d2397ff
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-18 09:07:05 -0800

    finishing Schema 4

--- Guatemala/BecaBrillante/db/schema/04_enrollment_by_typeofschool.sql
+++ Guatemala/BecaBrillante/db/schema/04_enrollment_by_typeofschool.sql
@@ -2,5 +2,8 @@ DROP TABLE IF EXISTS enrollment_by_typeofschool CASCADE;
 
 CREATE TABLE enrollment_by_typeofschool (
   typeofschool VARCHAR(50),
-
+  total INT,
+  man INT,
+  woman INT,
+  ignorado INT
 );
