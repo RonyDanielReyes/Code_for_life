2afddd10335f91c8afd68beb9e57421d41c54f20
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-18 09:02:36 -0800

    finishing query number 4

--- Guatemala/BecaBrillante/db/schema/04_enrollment_by_typeofschool.sql
+++ Guatemala/BecaBrillante/db/schema/04_enrollment_by_typeofschool.sql
@@ -2,4 +2,8 @@ DROP TABLE IF EXISTS enrollment_by_typeofschool CASCADE;
 
 CREATE TABLE enrollment_by_typeofschool (
   typeofschool VARCHAR(50),
+  official INT,
+  privada INT,  -- I did it privada instead of private because private is a reserved word in SQL
+  municipal INT,
+  cooperativa INT,
 );
