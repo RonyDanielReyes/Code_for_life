f3c69894c9efd0e8dec8229b1447e9473d61d8af
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-18 09:08:25 -0800

    creation of schema 5

--- Guatemala/BecaBrillante/db/schema/05_enrollement_by_areatype.sql
+++ Guatemala/BecaBrillante/db/schema/05_enrollement_by_areatype.sql
@@ -2,8 +2,9 @@ DROP TABLE IF EXISTS enrollment_by_areatype CASCADE;
 
 create table enrollment_by_areatype (
   areatype VARCHAR(50),
-  urban INT,
-  rural INT,
-  total INT
+  total INT,
+  man INT,
+  woman INT,
+  ignorado INT
 );
 ```
