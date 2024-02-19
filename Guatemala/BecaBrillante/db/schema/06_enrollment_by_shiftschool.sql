5d08cbd7053d4dc3e8ccbe01324415203a9de513
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-19 01:44:59 -0800

    finishing query 6

--- Guatemala/BecaBrillante/db/schema/06_enrollment_by_shiftschool.sql
+++ Guatemala/BecaBrillante/db/schema/06_enrollment_by_shiftschool.sql
@@ -1,2 +1,11 @@
 DROP TABLE IF EXISTS enrollment_by_shiftschool CASCADE;
 
+create table enrollment_by_shiftschool (
+  schooltype VARCHAR(50),
+  total INT,
+  Morning INT,
+  Afternoon INT,
+  overnight INT,
+  other INT,
+  ignorado INT
+  );
