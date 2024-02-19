1049815aadf6bd8e93b615f3e49d01e8d06e6db0
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-19 02:01:59 -0800

    finishing squema 8

--- Guatemala/BecaBrillante/db/schema/08_enrollment_kindergarten_bydepartment.sql
+++ Guatemala/BecaBrillante/db/schema/08_enrollment_kindergarten_bydepartment.sql
@@ -2,4 +2,9 @@ DROP TABLE IF EXISTS enrollment_kindergarten_bydepartment CASCADE;
 
 CREATE TABLE enrollment_kindergarten_bydepartment (
   department VARCHAR(50),
-  total INT,
+  year_2018 INT,
+  year_2019 INT,
+  year_2020 INT,
+  year_2021 INT,
+  year_2022 INT
+);
