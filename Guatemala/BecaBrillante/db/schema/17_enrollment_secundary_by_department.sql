d6fe11b08b9f852fd4b942d2d82f812e2b4a28aa
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-20 15:22:13 -0800

    adding squema 17

---
+++ Guatemala/BecaBrillante/db/schema/17_enrollment_secundary_by_department.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS enrollment_secundary_bydepartment CASCADE;
+
+CREATE TABLE enrollment_secundary_bydepartment (
+  department VARCHAR(50),
+  year_2018 INT,
+  year_2019 INT,
+  year_2020 INT,
+  year_2021 INT,
+  year_2022 INT
+);
