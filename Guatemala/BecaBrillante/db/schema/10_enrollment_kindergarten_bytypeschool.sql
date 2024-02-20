038464880a54e508198a5133e00307d07553a6ca
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-20 13:42:30 -0800

    adding schema 10

---
+++ Guatemala/BecaBrillante/db/schema/10_enrollment_kindergarten_bytypeschool.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS enrollment_kindergarten_bytypeschool CASCADE;
+
+CREATE TABLE enrollment_kindergarten_bytypeschool (
+  typeschool VARCHAR(50),
+  total INT,
+  man INT,
+  woman INT
+);
