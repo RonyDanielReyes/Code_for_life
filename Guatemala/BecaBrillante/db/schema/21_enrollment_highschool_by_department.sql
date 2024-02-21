df314d90d4d3e1f0c97b254c8627823d13b7328e
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-21 15:25:00 -0800

    adding squema 21

---
+++ Guatemala/BecaBrillante/db/schema/21_enrollment_highschool_by_department.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS enrollment_highschool_by_department CASCADE;
+
+CREATE TABLE enrollment_highschool_by_department (
+  department VARCHAR(50),
+  year_2018 INT,
+  year_2019 INT,
+  year_2020 INT
+  year_2021 INT
+  year_2022 INT
+);
