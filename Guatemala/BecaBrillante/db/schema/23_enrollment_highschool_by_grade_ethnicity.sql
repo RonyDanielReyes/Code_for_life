13de3a57283b19c19da65e84aa79787a340ff2b8
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-21 15:50:49 -0800

    adding squema 23

---
+++ Guatemala/BecaBrillante/db/schema/23_enrollment_highschool_by_grade_ethnicity.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS enrollment_highschool_by_grade;
+
+CREATE TABLE enrollment_highschool_by_grade (
+  grade VARCHAR(50),
+  total INT,
+  cuarto_grade INT,
+  quinto_grade INT,
+  sexto_grade INT,
+  septimo_grade INT
+);
