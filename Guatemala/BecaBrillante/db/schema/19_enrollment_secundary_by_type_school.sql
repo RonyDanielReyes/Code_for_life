47a489befd31df890ab5c295da235af01d5a02b0
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-20 19:35:53 -0800

    adding squema 19

---
+++ Guatemala/BecaBrillante/db/schema/19_enrollment_secundary_by_type_school.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS enrollment_secundary_by_type_school CASCADE;
+
+CREATE TABLE enrollment_secundary_by_type_school (
+  type_school VARCHAR(50),
+  total INT,
+  man INT,
+  woman INT
+);
