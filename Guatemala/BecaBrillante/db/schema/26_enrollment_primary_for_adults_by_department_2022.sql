29ca7d7048373a90f0013ed176e33a9f37ec494b
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-21 16:27:55 -0800

    adding squema 26

---
+++ Guatemala/BecaBrillante/db/schema/26_enrollment_primary_for_adults_by_department_2022.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS enrollment_primary_for_adults_by_department_2022 CASCADE;
+
+CREATE TABLE enrollment_primary_for_adults_by_department_2022 (
+  department VARCHAR(50),
+  amount INT
+);
