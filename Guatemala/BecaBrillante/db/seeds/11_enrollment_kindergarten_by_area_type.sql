55fc4dffae31ac628516afcce106250cc04002c2
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-23 15:37:51 -0800

    adding seed 11

---
+++ Guatemala/BecaBrillante/db/seeds/11_enrollment_kindergarten_by_area_type.sql
@@ -0,0 +1,5 @@
+INSERT INTO enrollment_kindergarten_by_area_type (area, total, man, woman)
+VALUES
+    ('Urbana', 189031, 94568, 94463),
+    ('Rural', 492016, 250387, 241629),
+    ('Ignorado', 17, NULL, 17);
