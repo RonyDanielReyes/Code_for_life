2ff4fb65eebbd09a59658db8999d01f71d8fce95
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-23 15:35:28 -0800

    adding seed 9

---
+++ Guatemala/BecaBrillante/db/seeds/09_enrollment_kindergarten_by_ethnicity.sql
@@ -0,0 +1,8 @@
+INSERT INTO enrollment_kindergarten_by_ethnicity (ethnicity, total, man, woman)
+VALUES
+    ('Maya', 236156, 120142, 116014),
+    ('Garífuna', 154, 73, 81),
+    ('Xinka', 210, 99, 111),
+    ('Mestizo / Ladino', 443743, 224258, 219485),
+    ('Otro', 795, 381, 414),
+    ('Ignorado', 6, 2, 4);
