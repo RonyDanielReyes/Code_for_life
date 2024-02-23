93d98e9f33a2df839dc9ded24ef07de274f0671b
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-23 15:30:04 -0800

    adding seed 7

---
+++ Guatemala/BecaBrillante/db/seeds/07_enrollment_data_by_shift_grades.sql
@@ -0,0 +1,7 @@
+INSERT INTO enrollment_by_shift_grades (grade, total, morning, afternoon, overnight, other, ignorado)
+VALUES
+    ('kindergarten', 681064, 600421, 35486, 0, 9796, 35361),
+    ('primary', 2335601, 2169136, 163278, 11, 3176, 0),
+    ('secundary', 675567, 249178, 356576, 9212, 59633, 968),
+    ('highschool', 350308, 119911, 126339, 3581, 97990, 2487),
+    ('primary for adults', 7273, 1133, 51, 4565, 1524, 0);
