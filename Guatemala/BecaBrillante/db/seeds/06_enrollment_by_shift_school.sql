5d8fd07bbf77920b4c1e22bd0c8335cf0cabfbc6
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-23 15:26:51 -0800

    adding seed 6

---
+++ Guatemala/BecaBrillante/db/seeds/06_enrollment_by_shift_school.sql
@@ -0,0 +1,6 @@
+INSERT INTO enrollment_by_shift_school (schooltype, total, morning, afternoon, overnight, other, ignorado)
+VALUES
+    ('Oficial', 3063920, 2581759, 402842, 11205, 32565, 35549),
+    ('Privado', 809075, 544632, 127225, 1092, 132903, 3223),
+    ('Municipal', 15623, 4351, 5417, 967, 4844, 44),
+    ('Cooperativa', 161195, 9037, 146246, 4105, 1807, 0);
