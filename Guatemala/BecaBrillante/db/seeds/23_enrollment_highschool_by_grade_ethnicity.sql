cdac0346cb466e5d6d69459c6837abe678978c23
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-24 10:04:12 -0800

    seed 23

---
+++ Guatemala/BecaBrillante/db/seeds/23_enrollment_highschool_by_grade_ethnicity.sql
@@ -0,0 +1,8 @@
+INSERT INTO enrollment_highschool_by_grade_ethnicity (grade, total, cuarto_grade, quinto_grade, sexto_grade, septimo_grade)
+VALUES
+    ('Maya', 69100, 30152, 26545, 12123, 280),
+    ('Garífuna', 199, 82, 87, 28, 2),
+    ('Xinka', 24, 6, 15, 3, 0),
+    ('Mestizo / Ladino', 280511, 123143, 115612, 40880, 876),
+    ('Otro', 422, 220, 186, 16, 0),
+    ('Ignorado', 52, 34, 13, 5, 0);
