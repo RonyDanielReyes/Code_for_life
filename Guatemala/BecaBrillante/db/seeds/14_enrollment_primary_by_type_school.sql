1bda79b3cba07c3e0eaa6f1cd70dfb1ea0061de4
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-24 09:30:54 -0800

    adding seed 14

---
+++ Guatemala/BecaBrillante/db/seeds/14_enrollment_primary_by_type_school.sql
@@ -0,0 +1,5 @@
+INSERT INTO enrollment_primary_by_type_school (type_school, total, man, woman)
+VALUES
+    ('Oficial', 2068946, 1061837, 1007109),
+    ('Privado', 264021, 135122, 128899),
+    ('Municipal', 2634, 1364, 1270);
