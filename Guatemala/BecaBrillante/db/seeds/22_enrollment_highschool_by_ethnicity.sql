750c4fb0aa928d9b13929a98d1b046655c007244
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-24 10:02:02 -0800

    adding seed 22

---
+++ Guatemala/BecaBrillante/db/seeds/22_enrollment_highschool_by_ethnicity.sql
@@ -0,0 +1,8 @@
+INSERT INTO enrollment_highschool_by_ethnicity (ethnicity, total, man, woman, ignorado)
+VALUES 
+    ('Maya', 69100, 33895, 35205, 0),
+    ('Garífuna', 199, 94, 105, 0),
+    ('Xinka', 24, 8, 16, 0),
+    ('Mestizo / Ladino', 280511, 129634, 150877, 0),
+    ('Otro', 422, 208, 214, 0),
+    ('Ignorado', 52, 15, 21, 16);
