7c70366581512f62d79e1e0a8a873c8bcbf6af47
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-24 09:42:26 -0800

    fixing squema 19

--- Guatemala/BecaBrillante/db/schema/19_enrollment_secundary_by_type_school.sql
+++ Guatemala/BecaBrillante/db/schema/19_enrollment_secundary_by_type_school.sql
@@ -3,7 +3,6 @@ DROP TABLE IF EXISTS enrollment_secundary_by_type_school CASCADE;
 CREATE TABLE enrollment_secundary_by_type_school (
   type_school VARCHAR(50),
   total INT,
-  year_2018 INT,
   man INT,
   woman INT
 );
