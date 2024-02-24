1f0f917f4cefc379ba4ce5851097323b920dbc48
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-24 10:31:09 -0800

    adding seed 28

---
+++ Guatemala/BecaBrillante/db/seeds/28_enrollment_primary_for_adults_by_area_type.sql
@@ -0,0 +1,5 @@
+INSERT INTO enrollment_primary_for_adults_by_area_type (area, total, man, woman)
+VALUES
+    ('Urbana', 6806, 3135, 3671),
+    ('Rural', 459, 201, 258),
+    ('Ignorado', 8, 2, 6);
