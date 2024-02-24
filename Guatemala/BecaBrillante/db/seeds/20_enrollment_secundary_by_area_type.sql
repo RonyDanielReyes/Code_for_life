0d62147c5f7b41ef9ab3221b76c36436cd77e0ad
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-24 09:58:34 -0800

    seed 20

---
+++ Guatemala/BecaBrillante/db/seeds/20_enrollment_secundary_by_area_type.sql
@@ -0,0 +1,5 @@
+INSERT INTO enrollment_secundary_by_area_type (area, total, man, woman)
+VALUES
+    ('Urbana', 402896, 202113, 200783),
+    ('Rural', 272436, 142517, 129919),
+    ('Ignorado', 235, 125, 110);
