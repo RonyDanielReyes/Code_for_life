b9b8112fec431170885c63a971cf6db8d5a1af55
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-26 11:36:27 -0800

    organizing in a better way the code

---
+++ Daily_practices/26_02_2024_1.js
@@ -0,0 +1,24 @@
+// exercise using the if statement
+
+// society is facing a new kind of problem, choice overload, You have so many choices today, that making decisions can be too dificult, Let's help people who are trying to decide
+// which school to send their children to by writing code for them
+
+// Elementary School if age is below 13
+// Secondary School if age is between 13 and 18 (both inclusive)
+// Lighthouse Labs in all other cases
+
+
+// challenge: 
+// the number is going to be type into the terminal console
+
+let age = 19;
+
+if (age < 13) {
+console.log("Elementary School");
+} else if (age >= 13 && age <= 18) {
+  console.log("Secondary School");
+} else {
+  console.log("Lighthouse Labs");
+}
+
+
