d5d1d5487d60672b118a726f8b0c5ceccd583de2
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-27 15:03:30 -0800

    adding the exercise number # 3

--- Daily_practices/27_02_2024_3.js
+++ Daily_practices/27_02_2024_3.js
@@ -1,9 +1,14 @@
 // questions file: working with loops for/while
 // question # 1, write a program that prints the sum of all odd numbers from 1 to 30 using a for loop.
 
+// initialize the variable to 0
 let sum = 0;
-for (let i = 1; i <= 9; i++) {
+
+// creating the for loop and going into the numbers between 1 and 30
+for (let i = 1; i <= 30; i++) {
+  // if the number is odd, add it to the sum
   if (i % 2 !== 0)
     sum += i;
 }
+
 console.log(sum);
