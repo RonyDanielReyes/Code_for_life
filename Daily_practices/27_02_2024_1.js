6c1f8ed1ffe77fbeb4379008b2d5510a5203fafa
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-27 12:18:30 -0800

    adding the resolution

--- Daily_practices/27_02_2024_1.js
+++ Daily_practices/27_02_2024_1.js
@@ -3,3 +3,14 @@
 // We are helping a famous singer with a song, the chorus has to be repeated 10 times, and the verse 3 times.
 // create everything with while loop
 
+chorus = "hey, hey, hey, hey, hey, hey, hey, hey, hey, hey"
+
+verse = "I'm sorry, the old Taylor can't come to the phone right now. Why? Oh, 'cause she's dead!"
+
+console.log(verse)
+let i = 0;
+while (i < 10) {
+    console.log(chorus)
+    i++
+}
+console.log(verse)
