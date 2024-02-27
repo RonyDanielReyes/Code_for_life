a3dc38251b430b82d7a059c04cfcd13549c85bcc
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-27 14:59:14 -0800

    adding the function resolution

---
+++ Daily_practices/27_02_2024_3.js
@@ -0,0 +1,9 @@
+// questions file: working with loops for/while
+// question # 1, write a program that prints the sum of all odd numbers from 1 to 30 using a for loop.
+
+let sum = 0;
+for (let i = 1; i <= 9; i++) {
+  if (i % 2 !== 0)
+    sum += i;
+}
+console.log(sum);
