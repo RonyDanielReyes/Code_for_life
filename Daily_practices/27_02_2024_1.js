5394b69ced67dbc74d3ffa8cdd05a9c822ba1593
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-27 12:13:51 -0800

    adding the challenge

--- Daily_practices/27_02_2024_1.js
+++ Daily_practices/27_02_2024_1.js
@@ -1,1 +1,5 @@
 // working with loops for/while 
+
+// We are helping a famous singer with a song, the chorus has to be repeated 10 times, and the verse 3 times.
+// create everything with while loop
+
