6ba55612c35ebded3f6294d354740da86c730e8d
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-27 12:19:56 -0800

    adding the final parts of the formula

--- Daily_practices/27_02_2024_1.js
+++ Daily_practices/27_02_2024_1.js
@@ -3,14 +3,16 @@
 // We are helping a famous singer with a song, the chorus has to be repeated 10 times, and the verse 3 times.
 // create everything with while loop
 
-chorus = "hey, hey, hey, hey, hey, hey, hey, hey, hey, hey"
+chorus = "hey"
 
-verse = "I'm sorry, the old Taylor can't come to the phone right now. Why? Oh, 'cause she's dead!"
+verse_1 = "I'm sorry, the old Taylor can't come to the phone right now. Why? Oh, 'cause she's dead!"
 
-console.log(verse)
+verse_2 = "I don't like your little games, don't like your tilted stage. The role you made me play, of the fool, no, I don't like you"
+
+console.log(verse_1)
 let i = 0;
 while (i < 10) {
     console.log(chorus)
     i++
 }
-console.log(verse)
+console.log(verse_2)
