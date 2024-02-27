f59d76e90ad332f370a20e2ac7acdd43c5f14ea0
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-27 14:50:57 -0800

    adding the code

--- Daily_practices/27_02_2024_2.js
+++ Daily_practices/27_02_2024_2.js
@@ -1,3 +1,9 @@
 // Working with for lops
 // // We are helping a famous singer with a song, the chorus has to be repeated 10 times, and the verse 3 times.
 // create everything with for loop
+
+chorus = "hey"
+for (let repeat = 0; repeat < 3; repeat++) {
+  console.log(chorus);
+}
+console.log("end of the song")
