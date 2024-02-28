c6c5fbda36e3db38f951c482cd767d3b7b5c800d
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-28 12:16:42 -0800

    adding the resolution to problem 4 from yesterday

--- Daily_practices/27_02_2024_4.js
+++ Daily_practices/27_02_2024_4.js
@@ -1,5 +1,7 @@
 // Write a program that prints the even numbers from 1 to 20 using a while loop.
 let num = 1;
-while () {
-  console.log(num);
-}
+while (num < 20) {
+  if (num % 2 === 0) {
+    console.log(num);
+  } num++;
+} 
