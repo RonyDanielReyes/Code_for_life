547542379af015e5c2a33241759d0f81aaea46ab
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-28 15:53:33 -0800

    adding first step solution

--- Daily_practices/28_02_2024.js
+++ Daily_practices/28_02_2024.js
@@ -1,8 +1,10 @@
 // You have been tasked by the Space Agency to design a simulator for an upcoming mission. Your task is to design a program to simulate the countdown of a rocket launch starting at 60. Your program will print the countdown to the console with the following conditions:
 
-// If the number is equal to 60, print the number followed by ": One Minute to Liftoff.".
-// If the number is greater than 10, print the number followed by ": All Systems Green.".
-// If the number is between 6 and 10, print the number followed by ": Starting Ignition Sequence.".
-// If the number is between 3 and 5, print the number followed by ": Ignition Complete.".
-// If the number is between 1 and 2, print the number followed by ": Full Power.".
-// If the number is equal to 0, print the number followed by ": Liftoff!" and then exit the program.
+/* If the number is equal to 60, print the number followed by ": One Minute to Liftoff.".
+If the number is greater than 10, print the number followed by ": All Systems Green.".
+If the number is between 6 and 10, print the number followed by ": Starting Ignition Sequence.".
+If the number is between 3 and 5, print the number followed by ": Ignition Complete.".
+If the number is between 1 and 2, print the number followed by ": Full Power.".
+If the number is equal to 0, print the number followed by ": Liftoff!" and then exit the program. */
+
+// first step writing a loop that counts down from 60 to 0
