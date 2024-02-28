bfbd288f5a38da02a98488e9c504c1419981a950
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-28 12:44:09 -0800

    adding the instructions for the codes

--- Daily_practices/27_02_2024_5.js
+++ Daily_practices/27_02_2024_5.js
@@ -1,6 +1,5 @@
 // Write a program that prints the numbers from 5 to 15 using a for loop.
 let num = 5;
-
 for (let i = 5; i <= 15; i++) {
   console.log(i);
 }
--- Daily_practices/27_02_2024_7.js
+++ Daily_practices/27_02_2024_7.js
@@ -1,1 +1,5 @@
 // Write a program that prints a countdown from 10 to 1 using a for loop.
+let num = 10;
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
