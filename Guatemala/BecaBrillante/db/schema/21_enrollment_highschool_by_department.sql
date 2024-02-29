8af5455f98e4933a45dba1b55c60b1508afd324c
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-28 17:20:03 -0800

    putting commas in a seed typo error

--- Guatemala/BecaBrillante/db/schema/21_enrollment_highschool_by_department.sql
+++ Guatemala/BecaBrillante/db/schema/21_enrollment_highschool_by_department.sql
@@ -4,7 +4,7 @@ CREATE TABLE enrollment_highschool_by_department (
   department VARCHAR(50),
   year_2018 INT,
   year_2019 INT,
-  year_2020 INT
-  year_2021 INT
+  year_2020 INT,
+  year_2021 INT,
   year_2022 INT
 );
