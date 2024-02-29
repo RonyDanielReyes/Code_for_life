d1c7d9f043e5f979a8cd6ee7ea08a0e12f07afbd
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-02-28 16:21:23 -0800

    finilizing challenge

--- Daily_practices/28_02_2024.js
+++ Daily_practices/28_02_2024.js
@@ -11,5 +11,17 @@ If the number is equal to 0, print the number followed by ": Liftoff!" and then 
 // second step write if statements for each condition
 
 for (let i = 60; i >= 0; i--) {
-
+  if (i === 60) {
+    console.log(i + ": One Minute to Liftoff.");
+  } else if (i >= 10) {
+    console.log(i + ": All Systems Green.");
+  } else if (i >= 6 && i <= 10) {
+    console.log(i + ": Starting Ignition Sequence.");
+  } else if (i >= 3 && i <= 5) {
+    console.log(i + ": : Ignition Complete.");
+  } else if (i >= 1 && i <= 2) {
+    console.log(i + ": Full Power.");
+  } else if (i === 0) {
+    console.log(i + ": Liftoff!");
+  } 
 }
