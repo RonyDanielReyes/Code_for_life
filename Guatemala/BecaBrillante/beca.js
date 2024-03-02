84fe9544c12b4de6f095bab863382e13e92085e5
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-02 11:13:38 -0800

    adding the pool.query function

--- Guatemala/BecaBrillante/beca.js
+++ Guatemala/BecaBrillante/beca.js
@@ -7,3 +7,12 @@ const pool = new Pool({
   database: 'guatemala'
 });
 
+pool.query(`
+SELECT * id, department, year_2018, year_2019, year_2020, year_2021, year_2022
+FROM enrollment_data
+LIMIT 10;
+`)
+.then(res=> {
+  console.log(res);
+})
+.catch(err => console.error('query error', err.stack));
