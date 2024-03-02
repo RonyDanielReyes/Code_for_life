18433458345483a06e27f8841d4829fe4776869a
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-02 11:29:42 -0800

    providing the query with objects

--- Guatemala/BecaBrillante/beca.js
+++ Guatemala/BecaBrillante/beca.js
@@ -1,18 +1,25 @@
-const {Pool} = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'labber',
-  password: 'labber',
-  host: 'localhost',
-  database: 'guatemala'
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "guatemala",
 });
 
-pool.query(`
+pool
+  .query(
+    `
 SELECT id, department, year_2018, year_2019, year_2020, year_2021, year_2022
 FROM enrollment_data
 LIMIT 24;
-`)
-.then(res=> {
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
+`
+  )
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(
+        `${row.id} ${row.department} ${row.year_2018} ${row.year_2019} ${row.year_2020} ${row.year_2021} ${row.year_2022}`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
