9fa489dabb42c2f2fba151f772e3b1ae220e206f
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-03 09:52:27 -0800

    adding the function of today

---
+++ Daily_practices/Pre/03_03_2024.js
@@ -0,0 +1,8 @@
+// working with arrays and for
+
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota', 'Nissan', 'Subaru', 'Hyundai', 'Kia', 'Mazda', 'Volkswagen', 'Audi', 'BMW', 'Mercedes', 'Porsche', 'Ferrari', 'Lamborghini', 'Maserati', 'Bugatti', 'McLaren', 'Koenigsegg'];
+
+console.log('Here are the cars I have owned:')
+for (let i = 0; i < cars.length; i++) {
+  console.log(`I want ${cars[i]}`)
+}
