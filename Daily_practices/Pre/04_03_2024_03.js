bd206678b050475b050eebb7a978576a3bfc9950
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-04 14:01:20 -0800

    adding challenge 1

--- Daily_practices/Pre/04_03_2024_03.js
+++ Daily_practices/Pre/04_03_2024_03.js
@@ -7,3 +7,8 @@ const film = {
 console.log(film.director); // Rony Reyes
 const somekey = 'director';
 console.log(film[somekey]); // Rony Reyes
+
+// adding a new key-value pair to the object
+film.year = 2001;
+
+console.log(film);
---
+++ Daily_practices/Pre/04_03_2024_04.js
@@ -0,0 +1,9 @@
+// creation of some exercises for objects
+
+/* Create an empty object called someone, add properties for name 
+and age after the object is created, then show the resulting object. */
+
+/* let someone = {};
+someone.name = 'Rony';
+someone.age = 30;
+console.log(someone); */
