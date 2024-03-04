0fe763b8027f3718161e4415886b690dd825ee3f
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-04 14:06:41 -0800

    adding solution for question 3

--- Daily_practices/Pre/04_03_2024_04.js
+++ Daily_practices/Pre/04_03_2024_04.js
@@ -12,13 +12,23 @@ console.log(someone); */
 /* exercise 2
 Create an object called gamer with properties for name, age, and highscore. 
 Update the highscore property after to 9001, then show the resulting object. */
-
-let gamer = {
+/* let gamer = {
   name: 'Rony',
   age: 30,
   highscore: 9000,
 };
-
 gamer.highscore = 9001;
+console.log(gamer); */
 
-console.log(gamer);
+// exercise 3
+/* Create an object called car with properties for make, model, and year. Remove the key year from the object, then show the resulting object */
+
+let car = {
+  make: 'Toyota',
+  model: 'Corolla',
+  year: 2020,
+};
+
+delete car.year;
+
+console.log(car);
