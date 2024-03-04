6496b5fdd378d5108615b19ebf5f4b864b7ab75a
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-04 14:04:12 -0800

    adding exercise 2

--- Daily_practices/Pre/04_03_2024_04.js
+++ Daily_practices/Pre/04_03_2024_04.js
@@ -1,5 +1,6 @@
 // creation of some exercises for objects
 
+// exercise 1
 /* Create an empty object called someone, add properties for name 
 and age after the object is created, then show the resulting object. */
 
@@ -7,3 +8,17 @@ and age after the object is created, then show the resulting object. */
 someone.name = 'Rony';
 someone.age = 30;
 console.log(someone); */
+
+/* exercise 2
+Create an object called gamer with properties for name, age, and highscore. 
+Update the highscore property after to 9001, then show the resulting object. */
+
+let gamer = {
+  name: 'Rony',
+  age: 30,
+  highscore: 9000,
+};
+
+gamer.highscore = 9001;
+
+console.log(gamer);
