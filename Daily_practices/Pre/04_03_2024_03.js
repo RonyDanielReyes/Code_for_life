03b951f868b1b6f7d452e733f750c091cc7ef4b4
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-04 13:50:50 -0800

    adding the funtionalities of it

---
+++ Daily_practices/Pre/04_03_2024_03.js
@@ -0,0 +1,9 @@
+// creationg of objects and working with objects
+const film = {
+  name: 'Harry Potter',
+  director: 'Rony Reyes',
+  series: "Indiana Jones",
+};
+console.log(film.director); // Rony Reyes
+const somekey = 'director';
+console.log(film[somekey]); // Rony Reyes
