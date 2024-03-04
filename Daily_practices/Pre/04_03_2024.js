384b678b292d1a65fd9789e32678c6c30f916f6a
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-04 10:34:25 -0800

    adding the file to work today

---
+++ Daily_practices/Pre/04_03_2024.js
@@ -0,0 +1,13 @@
+// working with while loop itinerating an array
+
+const packingList = [
+  "bowls",
+  "plates",
+  "pots",
+  "pans",
+  "eating utensils",
+  "glasses",
+  "cups",
+  "cooking utensils",
+];
+
