d4ce990c76c05c26fed9e75fb7e282f695123d09
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-05 11:35:35 -0800

    adding new challenge

---
+++ Daily_practices/Pre/05_03_2024_01.js
@@ -0,0 +1,15 @@
+// Imagine you have a grocery list 
+// of fruits with varying quantities and you
+// would like to know how many fruits in total there are.
+
+const fruits = {
+  apple: 42,
+  banana: 20,
+  cranberry: 13
+};
+
+let total = 0;
+for (const key in fruits){
+ total += fruits[key];
+}
+console.log(total);
