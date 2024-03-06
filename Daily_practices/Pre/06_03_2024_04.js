a203a1c1456ae24ce61593c3ee387a5ea1beea19
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-06 15:35:46 -0800

    adding the next challenge

---
+++ Daily_practices/Pre/06_03_2024_04.js
@@ -0,0 +1,10 @@
+// Loppy Lighthouse challenge
+// create a function with 3 parameters
+// range, multiples, words
+// range is an array of 2 numbers representing the start and end values for the loop
+// multiples is an array of 2 numbers representing the multiples you want to replace with words
+// words is an array of 2 strings representing the words that will replace the multiples
+
+function loopyLighthouse (range, multiples, words) {
+  
+}
