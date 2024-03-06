b9c1ad94fea7e1489ea0d06d261abf7c72d0e037
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-06 15:53:29 -0800

    adding the logic for the second part of th problem

--- Daily_practices/Pre/06_03_2024_04.js
+++ Daily_practices/Pre/06_03_2024_04.js
@@ -7,8 +7,13 @@
 
 function loopyLighthouse (range, multiples, words) {
   for (let i = range[0]; i <= range[1]; i++) {
-    console.log("i is " + i);
+    // console.log("i is " + i);
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    
+    }
   }
 }
 
-loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+loopyLighthouse([1, 90], [2, 5], ["Batty", "Beacon"]);
+
