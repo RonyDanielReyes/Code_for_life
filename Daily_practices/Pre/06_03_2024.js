b20040cbaba6f1eacdb5243ee2bc186ebc5c6ac8
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-06 08:00:19 -0800

    adding function to check if odd is working

--- Daily_practices/Pre/06_03_2024.js
+++ Daily_practices/Pre/06_03_2024.js
@@ -1,1 +1,14 @@
 // working on Odd Checker
+// create a function that checks if a number is odd or not
+// if the number is odd, return true
+// if the number is even, return false
+
+function isOdd(num) {
+  if (num % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log("Is odd " + isOdd(4));
