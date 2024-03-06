8bfcd82d6fa0dac7eff83cb065f3357f2da643d1
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-06 14:53:02 -0800

    adding the second challenge

--- Daily_practices/Pre/06_03_2024_02.js
+++ Daily_practices/Pre/06_03_2024_02.js
@@ -2,3 +2,9 @@
 //This database contains every user's date of birth, but people are finding it tricky to know someone's age 
 // using just their birth date. Your task is to write a function that can tell how old someone is based on their birth date.
 
+function ageCalculator (name, yearOfBirth, currentYear) {
+  return name + " is " + (currentYear - yearOfBirth) + " years old.";
+}
+
+
+console.log(ageCalculator("Suzie", 1983, 2015));
