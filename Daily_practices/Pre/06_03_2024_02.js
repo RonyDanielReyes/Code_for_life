10e3768a8fb504c6e2c5bc83b87b10148ae457d2
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-06 14:46:49 -0800

    adding the second challenge

---
+++ Daily_practices/Pre/06_03_2024_02.js
@@ -0,0 +1,4 @@
+// You've been given access to a database that contains all of the users for the PlentyOfDevs dating site. 
+//This database contains every user's date of birth, but people are finding it tricky to know someone's age 
+// using just their birth date. Your task is to write a function that can tell how old someone is based on their birth date.
+
