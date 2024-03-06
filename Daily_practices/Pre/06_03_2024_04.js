db70004c05d79d68380baa9ba41051328b328d53
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-06 15:45:25 -0800

    adding and testing the loop

--- Daily_practices/Pre/06_03_2024_04.js
+++ Daily_practices/Pre/06_03_2024_04.js
@@ -6,4 +6,9 @@
 // words is an array of 2 strings representing the words that will replace the multiples
 
 function loopyLighthouse (range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    console.log("i is " + i);
+  }
 }
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
