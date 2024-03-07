90a4351dc0a7cd51333e3b6ff2e31eee403c9f04
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-06 16:26:51 -0800

    finilizing challenge

--- Daily_practices/Pre/06_03_2024_05.js
+++ Daily_practices/Pre/06_03_2024_05.js
@@ -1,3 +1,14 @@
 // trash to treasure
 
+function smartGarbage(trash, bins) {
+  if (trash === 'recycling') {
+    bins.recycling++
+  } else if (trash === 'waste') {
+    bins.waste++
+  } else if (trash === 'compost') {
+    bins.compost++
+  }
+  return bins;
+}
 
+console.log(smartGarbage("compost", { waste: 1, recycling: 1, compost: 1 }));
