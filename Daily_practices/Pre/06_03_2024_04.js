949b380ca55ce505f16bf35aded8101e2f418992
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-06 16:04:45 -0800

    finalizing problem of today

--- Daily_practices/Pre/06_03_2024_04.js
+++ Daily_practices/Pre/06_03_2024_04.js
@@ -5,15 +5,19 @@
 // multiples is an array of 2 numbers representing the multiples you want to replace with words
 // words is an array of 2 strings representing the words that will replace the multiples
 
-function loopyLighthouse (range, multiples, words) {
+function loopyLighthouse(range, multiples, words) {
   for (let i = range[0]; i <= range[1]; i++) {
     // console.log("i is " + i);
     if (i % multiples[0] === 0 && i % multiples[1] === 0) {
       console.log(words[0] + words[1]);
-    
+    } else if (i % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
     }
   }
 }
 
 loopyLighthouse([1, 90], [2, 5], ["Batty", "Beacon"]);
-
