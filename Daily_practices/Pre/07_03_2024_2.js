ce34f286573d4c18a15e1ba7025bc4c2544fc969
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-11 21:09:04 -0700

    adding the logic to get into each element

--- Daily_practices/Pre/07_03_2024_2.js
+++ Daily_practices/Pre/07_03_2024_2.js
@@ -1,3 +1,9 @@
 // javascript Arrays
 
 const potatoRelatedEmoji = ["🥔", "🍠", "🍟"];
+
+[
+  "🥔", // The first item is at index: 0
+  "🍠", // The first item is at index: 1
+  "🍟", // The first item is at index: 2
+];
