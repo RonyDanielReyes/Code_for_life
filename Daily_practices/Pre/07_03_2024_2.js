60f11a3c3d119d927d8210cb55ce9fb500738946
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-11 21:06:12 -0700

    adding the const

--- Daily_practices/Pre/07_03_2024_2.js
+++ Daily_practices/Pre/07_03_2024_2.js
@@ -1,13 +1,3 @@
-// Javascript Arrays
-const potatoRelatedEmoji = ["🥔", "🍠", "🍟"];
+// javascript Arrays
 
-
-console.log("I like:", potatoRelatedEmoji[2]);
-// Output: I like: 
-
-// 🍟
-// Explanation: The third element of the array is "🍟" and it is printed to the console. Remember that arrays are zero-indexed, so the first element is at index 0, the second element is at index 1, and so on
-
-// Path: 07_03_2024_3.js
-// Javascript Arrays
 const potatoRelatedEmoji = ["🥔", "🍠", "🍟"];
