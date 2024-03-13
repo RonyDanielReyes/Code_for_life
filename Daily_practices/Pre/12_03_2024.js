4f30fc2d7b52670f2eb6dc786c4b49791e635438
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-12 21:59:15 -0700

    testing it on the console

--- Daily_practices/Pre/12_03_2024.js
+++ Daily_practices/Pre/12_03_2024.js
@@ -4,14 +4,14 @@
 // const student = ["Lora Baines", 28, "Lighthouse Labs", "JavaScript", "2023-07-09"];
 
 // better example:
-const student = {
+/* const student = {
   name: "Lora Baines", // A key-value pair in an object is called an object "property."
   age: 28,
   school: "Lighthouse Labs",
   favouriteLanguage: "JavaScript",
   startDate: "2023-07-09",
 };
-
+ */
 
 // console.log(student.name);
 
@@ -25,3 +25,5 @@ const student = {
 const catEmoji = ["🐱", "🐈", "😸", "😹", "😺", "😻", "😼", "😽", "😾", "😿", "🙀"];
 
 const numberofemojis = catEmoji.length;
+
+console.log("The number of cat emojis is: " + numberofemojis + " in it");
