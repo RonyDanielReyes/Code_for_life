5c1b93ac51ed86241c62913ccc24a7f268d81756
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-12 21:47:04 -0700

    adding the student variable

--- Daily_practices/Pre/12_03_2024.js
+++ Daily_practices/Pre/12_03_2024.js
@@ -2,3 +2,11 @@
 // working with javascript objects
 
 const student = ["Lora Baines", 28, "Lighthouse Labs", "JavaScript", "2023-07-09"];
+
+const student = {
+  name: "Lora Baines", // A key-value pair in an object is called an object "property."
+  age: 28,
+  school: "Lighthouse Labs",
+  favouriteLanguage: "JavaScript",
+  startDate: "2023-07-09",
+};
