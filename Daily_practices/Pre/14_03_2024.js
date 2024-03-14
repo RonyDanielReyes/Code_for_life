8eb207dbaab41da16ced76932f4e46844d3897ea
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-14 13:09:34 -0700

    adding the right information

--- Daily_practices/Pre/14_03_2024.js
+++ Daily_practices/Pre/14_03_2024.js
@@ -11,7 +11,8 @@ console.log(treeEmoji);
 // working with tre-dimensional arrays
 
 const onedimensional = [1, 2, 3];
-const twodimensional = [
+const twodimensional = 
+[
   [1, 2, 3], 
   [4, 5, 6]
   [7, 8, 9]
@@ -27,5 +28,6 @@ const threedimensional = [
     [10, 11, 12]
   ]
 ]; 
+
 
 
