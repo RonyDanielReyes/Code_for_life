47bb59b3964e5e393211884e35030c9770843f62
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-14 12:23:05 -0700

    adding the push ups

--- Daily_practices/Pre/14_03_2024.js
+++ Daily_practices/Pre/14_03_2024.js
@@ -1,2 +1,9 @@
 //  creating variable
 const treeEmoji = ["🌳"];
+// printing the array
+console.log(treeEmoji);
+// adding to the array a push method
+treeEmoji.push("🌲");
+treeEmoji.push("🌴");
+// priting the new array
+console.log(treeEmoji);
