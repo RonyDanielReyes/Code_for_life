454fc3be30f91bd27a8b04140b9b65d2de6f7108
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-14 12:25:36 -0700

    adding two dimensional

--- Daily_practices/Pre/14_03_2024.js
+++ Daily_practices/Pre/14_03_2024.js
@@ -1,5 +1,5 @@
 //  creating variable
-const treeEmoji = ["🌳"];
+/* const treeEmoji = ["🌳"];
 // printing the array
 console.log(treeEmoji);
 // adding to the array a push method
@@ -7,5 +7,11 @@ treeEmoji.push("🌲");
 treeEmoji.push("🌴");
 // priting the new array
 console.log(treeEmoji);
-
+ */
 // working with tre-dimensional arrays
+
+const onedimensional = [1, 2, 3];
+const twodimensional = [
+  [1, 2, 3], 
+  [4, 5, 6]
+];
