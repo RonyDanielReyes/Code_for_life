9b06277c97a1167fc24cd16877c39707f238ee26
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-15 15:27:59 -0700

    changing the value of an item

--- Python/Variables and Simple Data types/exercise.py
+++ Python/Variables and Simple Data types/exercise.py
@@ -5,3 +5,9 @@
 # learning about lists 15-03-2024
 bicyles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicyles[0])
+# accessing the last item
+print(bicyles[-1])
+
+# changing the value of an item in a list
+bicyles[0] = 'honda'
+print(bicyles[0])
