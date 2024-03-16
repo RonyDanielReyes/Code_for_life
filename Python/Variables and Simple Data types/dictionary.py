9043d501ea2ea767a2fccb67308d7754424b81f1
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-15 21:13:09 -0700

    trying to add some values

--- Python/Variables and Simple Data types/dictionary.py
+++ Python/Variables and Simple Data types/dictionary.py
@@ -9,6 +9,7 @@ alien_1['color'] = 'green'
 alien_1['points'] = 5
 alien_1['x_position'] = 0
 alien_1['y_position'] = 25
+alien_1['speed'] = 'medium'
 print(alien_1)
 
 # modifying values in a dictionary
@@ -16,5 +17,16 @@ alien_1['color'] = 'yellow'
 print(alien_1)
 
 # removing key-value pairs
-del alien_1['x_position']
+del alien_1['y_position']
+print(alien_1)
+
+# using get() to access values
+if alien_1['speed'] == 'slow':
+    x_increment = 1
+elif alien_1['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+    
+alien_1['x_position'] = alien_1['x_position'] + x_increment
 print(alien_1)
