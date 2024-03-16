eea5b80336915c8d7fd684d5c43f0a912c6cc465
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-15 21:03:11 -0700

    starting with an empty dictionary

--- Python/Variables and Simple Data types/dictionary.py
+++ Python/Variables and Simple Data types/dictionary.py
@@ -1,4 +1,12 @@
 ## working with dictionaries
-alien_0 = {'color': 'green', 'points': 5}
-print(alien_0['points'])
+""" alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['points']) """
 
+## starting with an empty dictionary
+alien_1 = {}
+
+alien_1['color'] = 'green'
+alien_1['points'] = 5
+alien_1['x_position'] = 0
+alien_1['y_position'] = 25
+print(alien_1)
