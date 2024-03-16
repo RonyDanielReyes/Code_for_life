9916b11664fc672b234c65ca5e7c8a78bb2bfb01
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-15 22:18:13 -0700

    adding the file

--- Python/Variables and Simple Data types/functions.py
+++ Python/Variables and Simple Data types/functions.py
@@ -1,1 +1,8 @@
 # working with functions
+def get_formated(first_name, last_name):
+  """Return a full name, neatly formatted."""
+  full_name = f"{first_name} {last_name}"
+  return full_name.title()
+
+musician = get_formated('jimi', 'hendrix')
+print(musician)
