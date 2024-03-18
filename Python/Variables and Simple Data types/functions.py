ac14f65043a947cd29193f8372700e24f4018af3
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-18 15:09:45 -0700

    commenting the other one

--- Python/Variables and Simple Data types/functions.py
+++ Python/Variables and Simple Data types/functions.py
@@ -1,11 +1,11 @@
 # Adding a new function
-def describe_pet(animal_type, pet_name):
+""" def describe_pet(animal_type, pet_name):
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
 describe_pet('hamster', 'harry')
 
-
+ """
 # Adding a new function
 def describe_pet(animal_type, pet_name):
     """Display information about a pet."""
