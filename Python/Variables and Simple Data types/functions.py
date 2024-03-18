6ba58a38914a389d2d641909572992776a8aca75
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-18 15:05:14 -0700

    adding the code of today

--- Python/Variables and Simple Data types/functions.py
+++ Python/Variables and Simple Data types/functions.py
@@ -1,5 +1,7 @@
-##  adding a new function
+# Adding a new function
 def describe_pet(animal_type, pet_name):
-  """Display information about a pet."""
-  print(f"\nI have a {animal_type}.")
-  print(f"My {animal_type}'s name is {pet_name.title()}.")
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('hamster', 'harry')
