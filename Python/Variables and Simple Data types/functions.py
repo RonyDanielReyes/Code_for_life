31bce687b626f11bbbca6ea4e24a71442c835732
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-18 19:47:44 -0700

    fixing the status

--- Python/Variables and Simple Data types/functions.py
+++ Python/Variables and Simple Data types/functions.py
@@ -7,10 +7,28 @@ describe_pet('hamster', 'harry')
 
  """
 # Adding a new function
-def describe_pet(animal_type, pet_name):
-    """Display information about a pet."""
+
+""" def describe_pet(animal_type, pet_name):
     print("\nI have a {}.".format(animal_type))
     print("My {}'s name is {}.".format(animal_type, pet_name.title()))
 
-describe_pet('hamster', 'harry')
-    
+describe_pet('hamster', 'harry') """
+
+# creating two functions can work together
+
+def print_modesls(unprinted_designs, completed_models):
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print("Printing model:{}.".format())
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+      
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_modesls(unprinted_designs, completed_models)
+show_completed_models(completed_models)
