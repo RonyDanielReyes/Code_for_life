4d4d70ebd7924187f17f9ff16b096685fa4acfe1
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-18 21:46:47 -0700

    commenting the code

--- Python/Variables and Simple Data types/classes.py
+++ Python/Variables and Simple Data types/classes.py
@@ -9,13 +9,21 @@ class Dog:
     # initialize name and age attributes
     self.name = name
     self.age = age
-    
+  
+  # creating a method
   def sit(self):
+    
+    # simulate a dog sitting in response to a command
     print("{} is now sitting.".format(self.name))
     
+  # creating a method
   def roll_over(self):
+    # simulate a dog rolling over in response to a command
     print("{} rolled over!".format(self.name))
     
+# creating an instance
 my_dog = Dog('willie', 6)
+# accessing attributes
 my_dog.sit()
+# accessing attributes
 my_dog.roll_over()
