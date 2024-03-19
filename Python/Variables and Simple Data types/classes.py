d3fd7e973f5f9c080b69ee5b038b83b37a53a7e7
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-19 10:54:26 -0700

    adding the odometer adding in the page

--- Python/Variables and Simple Data types/classes.py
+++ Python/Variables and Simple Data types/classes.py
@@ -39,15 +39,20 @@ your_dog.roll_over()
  """
  
 class Car:
-   def __init__(self, make, model, year):
+  def __init__(self, make, model, year):
       self.make = make
       self.model = model
       self.year = year
       self.odometer_reading = 0
     
-   def get_descriptive_name(self):
+  def get_descriptive_name(self):
     long_name = str(self.year) + ' ' + self.make + ' ' + self.model
     return long_name.title()
+  
+  def read_odometer(self):
+      print("This car has {} miles on it.".format(self.odometer_reading))
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
