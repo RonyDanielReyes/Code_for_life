df08e6176370bd46209cbbede693627adcace4e2
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-18 21:45:49 -0700

    adding for my understanding

--- Python/Variables and Simple Data types/classes.py
+++ Python/Variables and Simple Data types/classes.py
@@ -1,7 +1,12 @@
 # learning about classes 03/18/2024
+
+# creating a class
 class Dog:
   
+# a simple attempt to model a dog
   def __init__(self, name, age):
+    
+    # initialize name and age attributes
     self.name = name
     self.age = age
     
