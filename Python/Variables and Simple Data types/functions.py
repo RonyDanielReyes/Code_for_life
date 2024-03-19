a6d6ba7924372674ab63a22c173278c86ca98c2a
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-18 21:19:41 -0700

    understanding the logic of the functions

--- Python/Variables and Simple Data types/functions.py
+++ Python/Variables and Simple Data types/functions.py
@@ -16,10 +16,14 @@ describe_pet('hamster', 'harry') """
 
 # creating two functions can work together
 
-def print_modesls(unprinted_designs, completed_models):
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+def print_models(unprinted_designs, completed_models):
     while unprinted_designs:
         current_design = unprinted_designs.pop()
-        print("Printing model:{}.".format())
+        print("")
+        print("Printing model: {}.".format(current_design))
         completed_models.append(current_design)
 
 def show_completed_models(completed_models):
@@ -27,8 +31,6 @@ def show_completed_models(completed_models):
     for completed_model in completed_models:
         print(completed_model)
       
-unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
-completed_models = []
 
-print_modesls(unprinted_designs, completed_models)
+print_models(unprinted_designs, completed_models)
 show_completed_models(completed_models)
