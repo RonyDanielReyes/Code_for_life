6d0386b5746311f32ae6d3fb5ea86f53f8b29c65
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-19 09:55:45 -0700

    adding the self odometer

--- Python/Variables and Simple Data types/classes.py
+++ Python/Variables and Simple Data types/classes.py
@@ -43,6 +43,7 @@ class Car:
       self.make = make
       self.model = model
       self.year = year
+      self.odometer_reading = 0
     
    def get_descriptive_name(self):
     long_name = str(self.year) + ' ' + self.make + ' ' + self.model
