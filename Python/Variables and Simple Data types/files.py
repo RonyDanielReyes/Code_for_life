066685cab94d0c091964bf64e095665b3709ef60
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-21 14:58:17 -0700

    working with files

--- Python/Variables and Simple Data types/files.py
+++ Python/Variables and Simple Data types/files.py
@@ -1,1 +1,10 @@
 # 03-21-2024
+
+# python has to modify the file in order to read it
+
+# open the file in the same directory
+with open('file.txt') as file_object:
+  # read the file
+    contents = file_object.read()
+    # print the file
+    print(contents)
