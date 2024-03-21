e870209b6a7fcee334708f20b2d41e496064cf88
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-21 11:23:21 -0700

    printing the range

--- Python/Variables and Simple Data types/classes.py
+++ Python/Variables and Simple Data types/classes.py
@@ -88,5 +88,6 @@ class ElectricCar(Car):
 my_tesla = ElectricCar('tesla', 'models', 2019)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
 
 
