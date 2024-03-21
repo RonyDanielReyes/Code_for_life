99f2b488371d514973d9c7cbab414baafc2abe2b
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-21 12:40:00 -0700

    adding the files I need to add

---
+++ Python/Variables and Simple Data types/my_car.py
@@ -0,0 +1,11 @@
+# we can import files that are located in di
+# fferent directories by using sys.path.append
+
+from classes import Car
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
