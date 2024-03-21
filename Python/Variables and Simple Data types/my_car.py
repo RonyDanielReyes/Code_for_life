91db514b020e37d5ab9b3aeaec5c097ca1988b6e
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-21 14:40:45 -0700

    making it my own

--- Python/Variables and Simple Data types/my_car.py
+++ Python/Variables and Simple Data types/my_car.py
@@ -1,11 +1,21 @@
 # we can import files that are located in di
 # fferent directories by using sys.path.append
 
-from classes import Car
+""" from classes import Car
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 
 my_new_car.odometer_reading = 23
+my_new_car.read_odometer() """
+
+# importing and making an alias
+from classes import Car as EC
+
+my_new_car = EC('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
 
