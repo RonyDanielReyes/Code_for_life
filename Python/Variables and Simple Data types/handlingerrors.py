75e75971809e91b49d0318ce35e208e42bd29eb6
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-22 11:01:11 -0700

    adding the function to count the amount of words

--- Python/Variables and Simple Data types/handlingerrors.py
+++ Python/Variables and Simple Data types/handlingerrors.py
@@ -32,7 +32,7 @@ while True:
  """
 
 # codecs module is used to handle errors
-# this is with handling error
+""" # this is with handling error
 import codecs
 
 file = 'alice.txt'
@@ -40,6 +40,26 @@ try:
     with codecs.open(file, 'r', encoding='utf-8') as f:
         contents = f.read()
 except IOError:
+    print("Sorry, the file '{}' does not exist.".format(file))
+except Exception as e:
+    print("An error occurred:", e)
+ """
+ 
+ # adding the function split
+
+import codecs
+
+file = 'file.txt'
+try:
+    with codecs.open(file, 'r', encoding='utf-8') as f:
+        contents = f.read()
+except IOError:
     print("Sorry, the file '{}' does not exist or could not be opened.".format(file))
 except Exception as e:
     print("An error occurred:", e)
+    
+else:
+    words = contents.split()
+    num_words = len(words)
+    print("The file '{}' has about {} words.".format(file, num_words))
+
