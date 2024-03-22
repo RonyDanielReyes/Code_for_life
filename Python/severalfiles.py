689eaf3cf538df41dbc7f5772d04593835b012be
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-22 13:06:46 -0700

    adding the file in a functiion

--- Python/severalfiles.py
+++ Python/severalfiles.py
@@ -3,16 +3,16 @@ import codecs
 
 file = 'file.txt'
 
-try:
-    with codecs.open(file, 'r', encoding='utf-8') as f:
-        contents = f.read()
-except IOError:
-    print("Sorry, the file '{}' does not exist or could not be opened.".format(file))
-except Exception as e:
-    print("An error occurred:", e)
-    
-else:
-    words = contents.split()
-    num_words = len(words)
-    print("The file '{}' has about {} words.".format(file, num_words))
+def count_words(file):
+    try:
+        with codecs.open(file, 'r', encoding='utf-8') as f:
+            contents = f.read()
+    except IOError:
+        print("Sorry, the file '{}' does not exist".format(file))
+    except Exception as e:
+        print("An error occurred:", e)
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print("The file '{}' has about {} words.".format(file, num_words))
 
