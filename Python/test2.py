69d6117f0f0013c0f6a10643f379174d8ffe37c8
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-25 14:41:29 -0700

    adding import unitest

--- Python/test2.py
+++ Python/test2.py
@@ -1,11 +1,2 @@
-def main():
-    # Ask user for first name and last name
-    first_name = input("Enter your first name: ")
-    last_name = input("Enter your last name: ")
-
-    # Print welcome message with first name and last name
-    print("Welcome {}{}," .format(first_name, last_name))
-
-
-if __name__ == "__main__":
-    main()
+# import unitest module
+import unittest
