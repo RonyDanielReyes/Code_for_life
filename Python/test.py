e6b4c6940d1d7a9494f2322aadb94c366f7fa890
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-25 14:17:00 -0700

    addin the testing option

--- Python/test.py
+++ Python/test.py
@@ -1,16 +1,11 @@
 # testing a function
-
-
-## making a new line
-
 def get_formatted_name(first, last):
   full_name = first + ' ' + last
   return full_name.title()
 
 # testing the function
 print(get_formatted_name('jimi', 'hendrix'))
-# testing the function in different way
-print(get_formatted_name('john', 'lennon'))
+
 
 
 
--- Python/test1.py
+++ Python/test1.py
@@ -1,2 +1,14 @@
 # importing the test from another file
 from test import get_formatted_name
+
+print("Enter 'q' at any time to quit")
+while True:
+  first = input("\nPlease give me a first name: ")
+  if first == 'q':
+    break
+  last = input("Please give me a last name: ")
+  if last == 'q':
+    break
+  
+  formatted_name = get_formatted_name(first, last)
+  print("\tNeatly formatted name: " + formatted_name + '.')
