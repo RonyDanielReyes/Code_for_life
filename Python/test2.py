f14258b4db99ac7487dd5833173884c272dd0303
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-25 14:50:39 -0700

    adding the test function

--- Python/test2.py
+++ Python/test2.py
@@ -1,2 +1,12 @@
 # import unitest module
 import unittest
+from test import get_formatted_name
+
+# create a class that inherits from unittest.TestCase
+class NameTestCase(unittest.TestCase):
+    def test_first_last_name(self):
+        formatted_name = get_formatted_name('jimi', 'hendrix')
+        self.assertEqual(formatted_name, 'Jimi Hendrix')
+        
+if __name__ == '__main__':
+    unittest.main()
