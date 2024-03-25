1e017b2855a78675c6ed501178c9577a25c295c9
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-24 18:57:58 -0700

    adding the test functino

--- Python/test.py
+++ Python/test.py
@@ -2,6 +2,10 @@
 
 
 ## making a new line
+
 def get_formatted_name(first, last):
   full_name = first + ' ' + last
   return full_name.title()
+
+# testing the function
+print(get_formatted_name('jimi', 'hendrix'))
