b4be8a55bdb09089c1b288d2172035af8160ba67
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-03-25 15:03:47 -0700

    call the function and store the result

--- Python/test2.py
+++ Python/test2.py
@@ -6,6 +6,7 @@ from test import get_formatted_name
 class NameTestCase(unittest.TestCase):
     # create a method that tests the function
     def test_first_last_name(self):
+        # call the function and store the result
         formatted_name = get_formatted_name('jimi', 'hendrix')
         self.assertEqual(formatted_name, 'Jimi Hendrix')
         
